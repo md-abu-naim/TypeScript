468865cd83da0702ff691e427653ad65e6b2210b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-22 22:22:56 +0600

    jfsad

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-if ("" == 0) {
-    // It is! But why??
-  }
-  if (1 < x < 3) {
-    // True for *any* value of x!
-  }
-
-  
+// @ts-check
+/** @type {number} */
+var x;
+ 
+x = 0; // OK
+x = false; // Not OK
