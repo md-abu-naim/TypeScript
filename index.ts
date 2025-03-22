b9d551560cd284118c8ee96d7ffe6e2b13f255a7
abunaim34 <farhanadnanfarabi@gmail.com>
2025-03-22 22:24:11 +0600

    'fssad'

--- index.ts
+++ index.ts
@@ -12,3 +12,17 @@ var x;
  
 x = 0; // OK
 x = false; // Not OK
+
+
+class C {
+    constructor() {
+      /** @type {number | undefined} */
+      this.prop = undefined;
+      /** @type {number | undefined} */
+      this.count;
+    }
+  }
+   
+  let c = new C();
+  c.prop = 0; // OK
+  c.count = "string";
